67ffb1f9343394ea90b41765e490c29a8582792b
marvic <vic.solis.maria@gmail.com>
2022-05-29 15:07:18 -0400

    setting up

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,15 @@
+//require util
+const util = require('util')
 //require fs
 const fs = require('fs');
 //require path
 const path = require('path');
-//port
-const PORT = 3001;
 //require express and naming it app
 const express = require('express');
 const app = express();
+//port
+const PORT = 3001;
 //naming notes folder
 const notesFolder = require('./db/db.json')
+
+
